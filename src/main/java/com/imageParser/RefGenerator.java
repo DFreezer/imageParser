71638f767252b3b541db0ce733c7e67987d21bd2
Danil <freezer314@gmail.com>
2018-12-28 13:31:26 +0200

    Added proxy support

--- src/main/java/com/imageParser/RefGenerator.java
+++ src/main/java/com/imageParser/RefGenerator.java
@@ -6,6 +6,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.*;
+import java.net.*;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -22,14 +23,14 @@ public class RefGenerator {
         return urls;
     }
 
-    public static void generateParallel(int count) {
+    public static void generateParallel(int count, int nThreads, Proxy proxy) {
         String[] urls = generateUrls(count);
-        ExecutorService executorService = Executors.newFixedThreadPool(16);
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
         List<Future<?>> futures = new ArrayList<>();
         for (String url : urls) {
             Future<?> future = executorService.submit(() -> {
                 try {
-                    parseImageToFile(url);
+                    parseImageToFile(url, proxy);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -47,9 +48,11 @@ public class RefGenerator {
         }
     }
 
-    public static void parseImageToFile(String url) throws IOException {
+    public static void parseImageToFile(String url, Proxy proxy) throws IOException {
         Random random = new Random();
         Connection.Response response = Jsoup.connect(url)
+                .proxy(proxy)
+                .validateTLSCertificates(false)
                 .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
                 .ignoreHttpErrors(true)
                 .execute();
@@ -60,6 +63,8 @@ public class RefGenerator {
         if (!imgUrl.startsWith("//") && !imgUrl.isEmpty()) {
             System.out.println("valid!!!");
             Connection.Response imageResponse = Jsoup.connect(imgUrl)
+                    .proxy(proxy)
+                    .validateTLSCertificates(false)
                     .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
                     .cookies(cookies)
                     .ignoreContentType(true)
@@ -70,14 +75,20 @@ public class RefGenerator {
         }
     }
 
-
     public static void main(String[] args) {
         System.out.print("Enter urls count: ");
         Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
+        int count = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter threads: ");
+        int nThreads = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter hostname: ");
+        String hostname = scanner.nextLine();
+        System.out.print("Enter port: ");
+        int port = Integer.parseInt(scanner.nextLine());
         long start = System.nanoTime();
-        generateParallel(count);
+        generateParallel(count, nThreads, new Proxy(Proxy.Type.HTTP, new InetSocketAddress(hostname, port)));
         long end = System.nanoTime();
         System.out.println("Working time: " + (end - start)/1e+9 + " sec");
+        scanner.nextLine();
     }
 }
